991f3251ae565109be4cf94973004487941eb2b7
NazariiHolovatyi <holovatyi.nazarii@gmail.com>
2021-08-22 22:51:35 +0300

    Updates

---
+++ precache-manifest.b55831ada2eeefc38bc493fdd4b9e225.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1c8b29e7c9d08d64bff4",
+    "url": "/react-app/static/css/main.ef19f641.chunk.css"
+  },
+  {
+    "revision": "1c8b29e7c9d08d64bff4",
+    "url": "/react-app/static/js/main.7666f892.chunk.js"
+  },
+  {
+    "revision": "9ed022644c6f38b380ac",
+    "url": "/react-app/static/js/runtime~main.cbc86c42.js"
+  },
+  {
+    "revision": "4c59afacec54cb918c28",
+    "url": "/react-app/static/js/2.da5fdfa2.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6e65b90510ccecb5a9e4f93fb1b30323",
+    "url": "/react-app/index.html"
+  }
+];
